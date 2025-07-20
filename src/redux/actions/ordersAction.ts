bfe8ebb6ad7910b734f919cd6575058517d2a2cb
Bhavesh-Teli <telibhavesh11@gmail.com>
2025-07-20 23:11:43 +0530

    language change in view order in admin side

--- src/redux/actions/ordersAction.ts
+++ src/redux/actions/ordersAction.ts
@@ -6,12 +6,14 @@ interface IDate {
     fromDate: string;
     toDate: string;
     db_name?: string;
+    lang?: 'en' | 'hi' | 'gu';
 }
 
 
 export const fetchOrders = createAsyncThunk("fetchOrders",
     async (payload: IDate) => {
-        const res = await GetOrder(payload);
+        const lang = localStorage.getItem('appLanguage') as 'en' | 'hi' | 'gu'
+        const res = await GetOrder({ ...payload, lang });
         return res.data;
     }
 );
