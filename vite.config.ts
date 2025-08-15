7630779951b3e83e078faefa5dfbb22ecc60e332
thecodesoftsurat <thecodesoftsurat@gmail.com>
2025-08-15 20:48:03 +0530

    iis deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: '/', 
   server: {
     allowedHosts: ['active-civet-deadly.ngrok-free.app'],
     // other options like port, proxy, etc.
